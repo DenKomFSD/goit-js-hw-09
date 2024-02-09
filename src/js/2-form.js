949733b81467d3e76a25d307d9adb007e8ce76ca
iziKomSD <122015463+iziKomSD@users.noreply.github.com>
2024-02-09 19:07:56 +0200

    form task DONE

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const button = document.querySelector('button');
+
+function savedState(form) {
+  const userMail = form.email.value.trim();
+  const message = form.message.value.trim();
+  return {
+    userMail,
+    message,
+  };
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  localStorage.clear();
+  form.reset();
+});
+
+form.addEventListener('input', e => {
+  e.preventDefault();
+  const data = savedState(e.currentTarget);
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem('feedback-form-state', jsonData);
+});
+
+// const savedData = localStorage.getItem('feedback-form-state');
+// if (savedData) {
+//   const { userMail, message } = JSON.parse(savedData);
+
+//   form.email.value = userMail;
+//   form.message.value = message;
+// }
+
+function loadState() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const { userMail, message } = JSON.parse(savedData);
+
+    form.email.value = userMail;
+    form.message.value = message;
+  }
+}
+
+loadState();
